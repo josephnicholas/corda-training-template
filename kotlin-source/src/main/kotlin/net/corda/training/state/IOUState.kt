902f12b623491a5a85b54399cf076d29c9a7a265
Joseph Nicholas R. Alcantara <josephnicholasalcantara@gmail.com>
2019-09-02 10:52:26 +0800

    Initial commit on training updates

--- kotlin-source/src/main/kotlin/net/corda/training/state/IOUState.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/state/IOUState.kt
@@ -1,9 +1,10 @@
 package net.corda.training.state
 
-import net.corda.core.contracts.BelongsToContract
-import net.corda.core.contracts.ContractState
+import net.corda.core.contracts.*
 import net.corda.core.identity.Party
+import net.corda.finance.POUNDS
 import net.corda.training.contract.IOUContract
+import java.util.*
 
 /**
  * This is where you'll add the definition of your state object. Look at the unit tests in [IOUStateTests] for
@@ -12,6 +13,14 @@ import net.corda.training.contract.IOUContract
  * Remove the "val data: String = "data" property before starting the [IOUState] tasks.
  */
 @BelongsToContract(IOUContract::class)
-data class IOUState(val data: String = "data"): ContractState {
-    override val participants: List<Party> get() = listOf()
+data class IOUState(val amount: Amount<Currency>,
+                    val lender: Party,
+                    val borrower: Party,
+                    val paid: Amount<Currency> = Amount(0, amount.token),
+                    override val linearId: UniqueIdentifier = UniqueIdentifier()): LinearState {
+    override val participants: List<Party> get() = listOf(lender, borrower)
+
+    // Helper functions
+    fun pay(dollars: Amount<Currency>) = copy(paid = paid.plus(dollars));
+    fun withNewLender(newLender: Party) = copy(lender = newLender);
 }
