f0f1f2f3552efff037b439f4a9193fa495acab28
Joseph Nicholas R. Alcantara <josephnicholasalcantara@gmail.com>
2019-09-02 10:52:26 +0800

    Initial commit on training updates

--- kotlin-source/src/main/kotlin/net/corda/training/contract/IOUContract.kt
+++ kotlin-source/src/main/kotlin/net/corda/training/contract/IOUContract.kt
@@ -1,8 +1,6 @@
 package net.corda.training.contract
 
-import net.corda.core.contracts.CommandData
-import net.corda.core.contracts.Contract
-import net.corda.core.contracts.requireSingleCommand
+import net.corda.core.contracts.*
 import net.corda.core.transactions.LedgerTransaction
 import net.corda.training.state.IOUState
 
@@ -24,7 +22,7 @@ class IOUContract : Contract {
     interface Commands : CommandData {
         // Add commands here.
         // E.g
-        // class DoSomething : TypeOnlyCommandData(), Commands
+        class Issue: TypeOnlyCommandData(), CommandData
     }
 
     /**
@@ -32,6 +30,19 @@ class IOUContract : Contract {
      * The constraints are self documenting so don't require any additional explanation.
      */
     override fun verify(tx: LedgerTransaction) {
+        val refIssueCommand = tx.commands.requireSingleCommand<Commands.Issue>()
+
+        requireThat {
+            "No inputs should be consumed when issuing an IOU." using(tx.inputs.isEmpty())
+            "Only one output state should be created when issuing an IOU." using(tx.outputs.size == 1)
+            val refIOUState: IOUState = tx.outputStates.single() as IOUState;
+            "A newly issued IOU must have a positive amount." using(refIOUState.amount.quantity > 0)
+            "The lender and borrower cannot have the same identity." using(refIOUState.lender != refIOUState.borrower)
+            val setOfParticipants = refIssueCommand.signers.toSet();
+            "Both lender and borrower together only may sign IOU issue transaction." using(setOfParticipants == refIOUState.participants.map { it.owningKey }.toSet())
+        }
+
+
         // Add contract code here.
         // requireThat {
         //     ...
